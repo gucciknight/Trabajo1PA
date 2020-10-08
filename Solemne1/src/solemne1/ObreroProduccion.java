683bbf505534fbaa5cc3a97dc490029b9b7f7b81
Emilio Silva <emiliosilva.briones@gmail.com>
2020-10-08 19:51:31 -0300

    Sueldos para Obreros
    Se agrega la funcion de sueldos para los obreros

--- Solemne1/src/solemne1/ObreroProduccion.java
+++ Solemne1/src/solemne1/ObreroProduccion.java
@@ -17,8 +17,22 @@ public class ObreroProduccion extends Empleado{
     public ObreroProduccion(Boolean turnoDia, Boolean turnoTarde, Boolean turnoNoche, String rut, String nombre, String apellido, int diaFdN, int mesFdN, int anoFdN, String nivelEducativo){
     super(rut, nombre, apellido, diaFdN, mesFdN, anoFdN, nivelEducativo); 
     this.turnoDia = turnoDia;
-    this.turnoNoche = turnoNoche;
+    this.turnoTarde = turnoTarde;
     this.turnoNoche = turnoNoche;   
     this.esAdministrador = false;
+    }
+    
+    public double getSueldo(){
+        double sueldo = 0;
+        if(turnoDia = true){
+            sueldo = 250000;
+        }
+        if(turnoTarde = true){
+            sueldo = 350000;
+        }
+        if(turnoNoche =  true){
+            sueldo = 450000;
+        }
+        return sueldo;
     }
 }
