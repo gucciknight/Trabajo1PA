c966aeb74dbcb5a917b16530e700d52a5187344a
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 14:59:19 -0300

    Se crean vendedores y asistentes

--- Solemne1/src/solemne1/Solemne1.java
+++ Solemne1/src/solemne1/Solemne1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package solemne1;
-
+import solemne1.Asistente;
 /**
  *
  * @author Cristian
@@ -15,7 +15,12 @@ public class Solemne1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Se crean a los empleados
+        Asistente asistente1 = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
+        Asistente asistente2 = new Asistente("123456789", "asistente", "segundo", 02, 02, 2002, "Universitario");
+        Vendedor vendedor1 = new Vendedor("valparaiso", "124562346", "vendedor", "primero" , 02, 03, 04, "Tectico en invformatica San Sebastian");
+        Vendedor vendedor2 = new Vendedor("valparaiso", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tectico en invformatica U catolica");
+
     }
     
 }
