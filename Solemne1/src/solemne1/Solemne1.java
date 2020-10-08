330dda156a36fa9656313c2f305698e85123027f
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 18:20:40 -0300

    orden oficial de la lista

--- Solemne1/src/solemne1/Solemne1.java
+++ Solemne1/src/solemne1/Solemne1.java
@@ -17,22 +17,22 @@ public class Solemne1 {
     public static void main(String[] args) {
         //Se crean a los empleados
         Empleado Empleados[] = new Empleado[15];
-        Empleados[0] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[1] = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[2] = new Vendedor("RM", "124562346", "vendedor", "primero" , 02, 03, 04, "Tecnico en informatica San Sebastian");
-        Empleados[3] = new Vendedor("RM", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tecnico en informatica U Catolica");
-        Empleados[4] = new Vendedor("Zona Norte", "124562346", "vendedor", "tercero", 02, 06,04, "Tecnico en informatica Inacap");
-        Empleados[5] = new Vendedor("Zona Sur", "124562346", "vendedor", "cuarto", 02, 07, 04, "Tecnico en informatica AIEP");
-        Empleados[6] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
-        Empleados[7] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Segundo", 25, 05,04, "Educacion Media Completa" );
-        Empleados[8] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Tercero", 25, 05,04, "Educacion Media Completa" );
-        Empleados[9] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Cuarto", 25, 05,04, "Educacion Media Completa" );
-        Empleados[10] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Quinto", 25, 05,04, "Educacion Media Completa" );
-        Empleados[11] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
-        Empleados[12] = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-
-        Empleados[13] = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
-        Empleados[14] = new Asistente("123456789", "asistente", "segundo", 02, 02, 2002, "Universitario");
+        Empleados[0] = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        Empleados[1] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        Empleados[2] = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        Empleados[3] = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
+        Empleados[4] = new Vendedor("RM", "124562346", "vendedor", "primero" , 02, 03, 04, "Tecnico en informatica San Sebastian");
+        Empleados[5] = new Vendedor("RM", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tecnico en informatica U Catolica");
+        Empleados[6] = new Vendedor("Zona Norte", "124562346", "vendedor", "tercero", 02, 06,04, "Tecnico en informatica Inacap");
+        Empleados[7] = new Vendedor("Zona Sur", "124562346", "vendedor", "cuarto", 02, 07, 04, "Tecnico en informatica AIEP");
+        Empleados[8] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
+        Empleados[9] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Segundo", 25, 05,04, "Educacion Media Completa" );
+        Empleados[10] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Tercero", 25, 05,04, "Educacion Media Completa" );
+        Empleados[11] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Cuarto", 25, 05,04, "Educacion Media Completa" );
+        Empleados[12] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Quinto", 25, 05,04, "Educacion Media Completa" );
+        Empleados[13] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
+  
+        
         System.out.println(Empleados[13].nombre);
     }
     
