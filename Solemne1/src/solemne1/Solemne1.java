b76cba16b068ebb02dc1d0d637c1f76cd65022f6
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 18:15:15 -0300

    SE RESEULVE BUG

--- Solemne1/src/solemne1/Solemne1.java
+++ Solemne1/src/solemne1/Solemne1.java
@@ -17,8 +17,8 @@ public class Solemne1 {
     public static void main(String[] args) {
         //Se crean a los empleados
         Empleado Empleados[] = new Empleado[15];
-        Empleados[0] = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[1] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        Empleados[0] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        Empleados[1] = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
         Empleados[2] = new Vendedor("RM", "124562346", "vendedor", "primero" , 02, 03, 04, "Tecnico en informatica San Sebastian");
         Empleados[3] = new Vendedor("RM", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tecnico en informatica U Catolica");
         Empleados[4] = new Vendedor("Zona Norte", "124562346", "vendedor", "tercero", 02, 06,04, "Tecnico en informatica Inacap");
@@ -30,10 +30,9 @@ public class Solemne1 {
         Empleados[10] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Quinto", 25, 05,04, "Educacion Media Completa" );
         Empleados[11] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
         Empleados[12] = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[13] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[14] = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[15] = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
-        Empleados[16] = new Asistente("123456789", "asistente", "segundo", 02, 02, 2002, "Universitario");
+
+        Empleados[13] = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
+        Empleados[14] = new Asistente("123456789", "asistente", "segundo", 02, 02, 2002, "Universitario");
         System.out.println(Empleados[13].nombre);
     }
     
