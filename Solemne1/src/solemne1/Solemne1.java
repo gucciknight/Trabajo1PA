ea59c487dd176bbb446df3f846d94b7a7b3933ac
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 19:02:56 -0300

    Estructura oficial de lops empleados

--- Solemne1/src/solemne1/Solemne1.java
+++ Solemne1/src/solemne1/Solemne1.java
@@ -15,25 +15,33 @@ public class Solemne1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Se crean a los empleados
-        Empleado Empleados[] = new Empleado[15];
-        Empleados[0] = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[1] = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[2] = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
-        Empleados[3] = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
-        Empleados[4] = new Vendedor("RM", "124562346", "vendedor", "primero" , 02, 03, 04, "Tecnico en informatica San Sebastian");
-        Empleados[5] = new Vendedor("RM", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tecnico en informatica U Catolica");
-        Empleados[6] = new Vendedor("Zona Norte", "124562346", "vendedor", "tercero", 02, 06,04, "Tecnico en informatica Inacap");
-        Empleados[7] = new Vendedor("Zona Sur", "124562346", "vendedor", "cuarto", 02, 07, 04, "Tecnico en informatica AIEP");
-        Empleados[8] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
-        Empleados[9] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Segundo", 25, 05,04, "Educacion Media Completa" );
-        Empleados[10] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Tercero", 25, 05,04, "Educacion Media Completa" );
-        Empleados[11] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Cuarto", 25, 05,04, "Educacion Media Completa" );
-        Empleados[12] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Quinto", 25, 05,04, "Educacion Media Completa" );
-        Empleados[13] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
-  
         
-        //System.out.println(Empleados[13].nombre);
+        //Se crean a los empleados
+        Gerente elGerente = new Gerente("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        
+        ResponsableNomina elResponsabledeNomina = new ResponsableNomina("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        
+        Contador contador = new Contador("13467567k", "Emilio", "Silva", 01, 02, 1993, "Universitario");
+        
+        Asistente asistente = new Asistente("18392821k", "agustin", "rodriguez", 01, 02, 1993, "Universitario");
+        
+        Vendedor Vendedores[] = new Vendedor[4];
+        Vendedores[0] = new Vendedor("RM", "124562346", "vendedor", "primero" , 02, 03, 04, "Tecnico en informatica San Sebastian");
+        Vendedores[1] = new Vendedor("RM", "124562346", "vendedor", "segundo" , 02, 05, 04, "Tecnico en informatica U Catolica");
+        Vendedores[2] = new Vendedor("Zona Norte", "124562346", "vendedor", "tercero", 02, 06,04, "Tecnico en informatica Inacap");
+        Vendedores[3] = new Vendedor("Zona Sur", "124562346", "vendedor", "cuarto", 02, 07, 04, "Tecnico en informatica AIEP");
+        
+        ObreroProduccion obrerosProduccion[] = new ObreroProduccion[6]; 
+        obrerosProduccion[0] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
+        obrerosProduccion[1] = new ObreroProduccion(true, false, false,"124562346", "Obrero", "Segundo", 25, 05,04, "Educacion Media Completa" );
+        obrerosProduccion[2] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Tercero", 25, 05,04, "Educacion Media Completa" );
+        obrerosProduccion[3] = new ObreroProduccion(false, true, false,"124562346", "Obrero", "Cuarto", 25, 05,04, "Educacion Media Completa" );
+        obrerosProduccion[4] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Quinto", 25, 05,04, "Educacion Media Completa" );
+        obrerosProduccion[5] = new ObreroProduccion(false, false, true,"124562346", "Obrero", "Primero", 25, 05,04, "Educacion Media Completa" );
+        
+        //responsable de nomina, crea nomina
+        
+        elResponsabledeNomina.crearNomina(1);
     }
     
 }
