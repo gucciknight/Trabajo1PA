851d91dd7a2ca1919e4088f94ba2c0193743579e
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 10:48:45 -0300

    Update Vendedor.java
    se crea clase y constructor vendedor

--- Solemne1/src/solemne1/Vendedor.java
+++ Solemne1/src/solemne1/Vendedor.java
@@ -12,4 +12,10 @@ package solemne1;
 public class Vendedor extends Empleado{
     public String region;
     private int ventasmes;
+    
+    public Vendedor(String Region , int ventasMes, String rut, String nombre, String apellido, int diaFdN, int mesFdN, int anoFdN, String nivelEducativo, Boolean esAdministrador){
+    super(rut, nombre, apellido, diaFdN, mesFdN, anoFdN, nivelEducativo, esAdministrador); 
+    this.region = Region;
+    this.ventasmes = ventasMes;
+    }
 }
