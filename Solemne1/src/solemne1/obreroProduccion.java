b6dd103046bfc409a1f19b4a0c557b7aca696bf0
gucciknight <arodriguezl3@correo.uss.cl>
2020-10-08 10:46:53 -0300

    Create obreroProduccion.java
    se añade el obrero de produccion

---
+++ Solemne1/src/solemne1/obreroProduccion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solemne1;
+
+/**
+ *
+ * @author arluna
+ */
+public class obreroProduccion extends Empleado{
+    public Boolean turnoDia;
+    public Boolean turnoTarde;
+    public Boolean turnoNoche;
+    
+    public obreroProduccion(Boolean turnoDia, Boolean turnoTarde, Boolean turnoNoche, String rut, String nombre, String apellido, int diaFdN, int mesFdN, int anoFdN, String nivelEducativo, Boolean esAdministrador){
+    super(rut, nombre, apellido, diaFdN, mesFdN, anoFdN, nivelEducativo, esAdministrador); 
+    this.turnoDia = turnoDia;
+    this.turnoNoche = turnoNoche;
+    this.turnoNoche = turnoNoche;
+    }
+}
